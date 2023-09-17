4b073dc114dbb752b0189a0d206598ef24ca3596
Navec Batchi <navecbatchi@gmail.com>
2023-09-17 09:07:10 +0200

    chore(design): update design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,119 +1,180 @@
 import { faker } from "@faker-js/faker";
-import { AiTwotoneHeart } from "react-icons/ai";
-import { FaSearch } from "react-icons/fa";
-import { HiMenuAlt2 } from "react-icons/hi";
-import styles from "./index.module.css";
+import { FaListUl } from "react-icons/fa";
+import { FcMenu } from "react-icons/fc";
+import { MdOutlineComputer } from "react-icons/md";
+import { PiGridFourFill, PiHardDrivesFill } from "react-icons/pi";
+// const Home = () => {
+//   return (
+//     <section>
+//       <div style={{ position: "relative" }}>
+//         <div
+//           style={{
+//             background: "#3d7a65",
+//             position: "absolute",
+//             height: "calc(100% + 6rem)",
+//             width: "calc(100% + 2rem)",
+//             top: "-4rem",
+//             left: "-1rem",
+//           }}
+//         />
+//         <div
+//           style={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//             position: "relative",
+//           }}
+//         >
+//           <HiMenuAlt2
+//             style={{
+//               color: "#ffffff",
+//               position: "relative",
+//               fontSize: "1.5rem",
+//             }}
+//           />
+//           <FaSearch
+//             style={{
+//               color: "#ffffff",
+//               position: "relative",
+//               fontSize: "1.5rem",
+//             }}
+//           />
+//         </div>
+//         <h1 style={{ color: "#ffffff", position: "relative" }}>
+//           Mon nom de site
+//         </h1>
+
+//         <img
+//           style={{ position: "relative" }}
+//           src={faker.image.url()}
+//           alt="to be replace"
+//           className={styles.poster}
+//         />
+//       </div>
+
+//       <h2
+//         style={{
+//           borderBottom: "1px solid",
+//           marginTop: "3rem",
+//           color: "#3d7a65",
+//         }}
+//       >
+//         Produits
+//       </h2>
+//       <ul style={{ margin: 0, padding: 0 }}>
+//         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
+//           <li
+//             key={index}
+//             style={{
+//               boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+//               marginTop: "2rem",
+//               position: "relative",
+//             }}
+//           >
+//             <AiTwotoneHeart
+//               style={{
+//                 position: "absolute",
+//                 top: "0.5rem",
+//                 right: "0.5rem",
+//                 fontSize: "2rem",
+//                 color: "#aaa",
+//               }}
+//             />
+//             <img
+//               src={faker.image.url()}
+//               alt="to be replace"
+//               style={{ width: "100%" }}
+//             />
+//             <div style={{ padding: 10 }}>
+//               <div style={{ display: "flex", flexDirection: "column" }}>
+//                 <h3
+//                   style={{
+//                     fontSize: "1rem",
+//                     margin: "0 0 5px",
+//                   }}
+//                 >
+//                   LOREM IPSUM DOLOR
+//                 </h3>
+//                 <span style={{ color: "#777" }}>
+//                   Lorem ipsum dolor sit amet consectetur
+//                 </span>
+//                 <span
+//                   style={{
+//                     fontSize: 12,
+//                     display: "block",
+//                     textAlign: "right",
+//                     marginTop: 5,
+//                   }}
+//                 >
+//                   Il en reste 4 en stock
+//                 </span>
+//               </div>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// };
 
 const Home = () => {
   return (
-    <section>
-      <div style={{ position: "relative" }}>
-        <div
-          style={{
-            background: "#3d7a65",
-            position: "absolute",
-            height: "calc(100% + 6rem)",
-            width: "calc(100% + 2rem)",
-            top: "-4rem",
-            left: "-1rem",
-          }}
-        />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            position: "relative",
-          }}
-        >
-          <HiMenuAlt2
-            style={{
-              color: "#ffffff",
-              position: "relative",
-              fontSize: "1.5rem",
-            }}
-          />
-          <FaSearch
-            style={{
-              color: "#ffffff",
-              position: "relative",
-              fontSize: "1.5rem",
-            }}
-          />
-        </div>
-        <h1 style={{ color: "#ffffff", position: "relative" }}>
-          Mon nom de site
-        </h1>
-
-        <img
-          style={{ position: "relative" }}
-          src={faker.image.url()}
-          alt="to be replace"
-          className={styles.poster}
-        />
+    <div>
+      <div style={{ textAlign: "end" }}>
+        <FcMenu style={{ fontSize: "1.5rem" }} />
       </div>
 
-      <h2
-        style={{
-          borderBottom: "1px solid",
-          marginTop: "3rem",
-          color: "#3d7a65",
-        }}
-      >
-        Produits
-      </h2>
-      <ul style={{ margin: 0, padding: 0 }}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
-          <li
+      <h1>Explore</h1>
+
+      <div style={{ display: "flex", gap: 15 }}>
+        {[MdOutlineComputer, PiHardDrivesFill].map((Icon, index) => (
+          <div
             key={index}
             style={{
-              boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-              marginTop: "2rem",
-              position: "relative",
+              width: 75,
+              height: 75,
+              color: "#fff",
+              backgroundColor: "#3d7a65",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "50%",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
             }}
           >
-            <AiTwotoneHeart
-              style={{
-                position: "absolute",
-                top: "0.5rem",
-                right: "0.5rem",
-                fontSize: "2rem",
-                color: "#aaa",
-              }}
-            />
-            <img
-              src={faker.image.url()}
-              alt="to be replace"
-              style={{ width: "100%" }}
-            />
-            <div style={{ padding: 10 }}>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <h3
-                  style={{
-                    fontSize: "1rem",
-                    margin: "0 0 5px",
-                  }}
-                >
-                  LOREM IPSUM DOLOR
-                </h3>
-                <span style={{ color: "#777" }}>
-                  Lorem ipsum dolor sit amet consectetur
-                </span>
-                <span
-                  style={{
-                    fontSize: 12,
-                    display: "block",
-                    textAlign: "right",
-                    marginTop: 5,
-                  }}
-                >
-                  Il en reste 4 en stock
-                </span>
+            <Icon style={{ fontSize: "2rem" }} />
+          </div>
+        ))}
+      </div>
+
+      <div style={{ display: "flex", marginTop: "2rem" }}>
+        <div style={{ flex: 1 }}>xxxx products</div>
+        <div style={{ display: "flex", gap: 10 }}>
+          <FaListUl fontSize="1.2rem" />
+          <PiGridFourFill fontSize="1.2rem" />
+        </div>
+      </div>
+
+      <div>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
+          <div
+            key={index}
+            style={{
+              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+              marginTop: "2rem",
+            }}
+          >
+            <img src={faker.image.url()} alt="" style={{ width: "100%" }} />
+            <div style={{ padding: "0.5rem" }}>
+              <h3 style={{ margin: 0 }}>Lorem ipsum dolor</h3>
+              <div style={{ marginTop: 10, color: "#777" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
               </div>
             </div>
-          </li>
+          </div>
         ))}
-      </ul>
-    </section>
+      </div>
+    </div>
   );
 };
 
